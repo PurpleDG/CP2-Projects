bb4882a0cafeb01f4ef1f5700b1d9d019a4cb5a9
PurpleDG <evan.mccabe@ucas-edu.net>
2025-01-08 13:44:53 -0700

    I started working on a Financial Calculator program.

--- financialCalculator/main.py
+++ financialCalculator/main.py
@@ -1,4 +1,17 @@
 #Evan McCabe Financial Calculator
 
+
+
+#Introduction to the user:
+print("\nHello! I am a financial calculator.")
+
+#The main function that runs the program:
 def main():
-    print("SIGMA")
+    #Ask the user what function they would like the calculator to perform:
+    print("\nWhat function would you like me to perform?")
+    #Tell the user what their options are on the calculator:
+    print("1: Saving Goal\n2: Coumpound Interest Calculator\n3: Budget Allocator\n4: Sale Price Calculator\n5: Tip Calculator\n")
+
+    main()
+
+main()
