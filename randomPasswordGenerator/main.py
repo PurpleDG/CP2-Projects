d4da4b9d9855250c0554e99a54838079e9321f0d
PurpleDG <evan.mccabe@ucas-edu.net>
2025-01-22 13:44:18 -0700

    I started a random password generator program.

---
+++ randomPasswordGenerator/main.py
@@ -0,0 +1,36 @@
+# Evan McCabe Random Password Generator
+
+"""" HINT: You can make this by randomly selecting from different lists 
+     OR by randomly generating the ASCII letters! """
+
+print("\nHello! I am the random password generator.")
+
+def upperLetters():
+    
+    num = input("\n")
+
+def lowerLetters():
+    pass
+
+def numbers():
+    pass
+
+def specialCharacters():
+    pass
+
+def assemble():
+    pass
+
+def main():
+    
+    upperLetters()
+
+    lowerLetters()
+
+    numbers()
+
+    specialCharacters()
+
+    assemble()
+
+main()
