0c4122a638e4f22ba1e879a521f8a9f2d729b209
PurpleDG <evan.mccabe@ucas-edu.net>
2025-01-23 13:42:03 -0700

    I finished Pseudocode for another project and ocntinued working on this one.

--- randomPasswordGenerator/main.py
+++ randomPasswordGenerator/main.py
@@ -1,13 +1,24 @@
 # Evan McCabe Random Password Generator
 
-"""" HINT: You can make this by randomly selecting from different lists 
-     OR by randomly generating the ASCII letters! """
+import string
+
+lowerLetters = list(string.ascii_lowercase)
+
+upperLetters = list(string.ascii_uppercase)
+
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+specialCharacters = list(string.punctuation)
 
 print("\nHello! I am the random password generator.")
 
+def numLetters():
+
+    num = input("")
+
 def upperLetters():
     
-    num = input("\n")
+    yn = input("")
 
 def lowerLetters():
     pass
@@ -23,6 +34,8 @@ def assemble():
 
 def main():
     
+    numLetters()
+
     upperLetters()
 
     lowerLetters()
--- testCode/main.py
+++ testCode/main.py
@@ -1,1 +1,4 @@
 #This is where I test pieces of code before using them in an actual program.
+n = int(input("Number: "))
+n = n // 4
+print(n)
