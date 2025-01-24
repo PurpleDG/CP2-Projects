c86b53f9169ac033afd4ea5cd1d56df4cf0556e4
PurpleDG <evan.mccabe@ucas-edu.net>
2025-01-24 13:45:09 -0700

    I have almost finished my random password generator program, but I am running into errors.

--- randomPasswordGenerator/main.py
+++ randomPasswordGenerator/main.py
@@ -2,48 +2,109 @@
 
 import string
 
-lowerLetters = list(string.ascii_lowercase)
+import random
 
-upperLetters = list(string.ascii_uppercase)
+lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-specialCharacters = list(string.punctuation)
+specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
 
 print("\nHello! I am the random password generator.")
 
-def numLetters():
+def numLetters(options, optionsPicked, password):
 
-    num = input("")
+    num = input("\nHow many characters long do you want your password to be?\n")
 
-def upperLetters():
+    try:
+         num = int(num)
+    except ValueError:
+         print("\nINVALID INPUT\n\nPlease try again.")
+         numLetters()
+
+    upperLetters(num, options, optionsPicked, password)
+
+def upperLetters(num, options, optionsPicked, password):
     
-    yn = input("")
+    yn = input("\nWould you like to include uppercase letters in yourpassword? (y = yes, n = no)\n")
 
-def lowerLetters():
-    pass
+    if yn.upper() == "Y":
+        print("\nUppercase letters will be included in your password.")
+        options.append(upperLetters)
+        optionsPicked += 1
 
-def numbers():
-    pass
+    else:
+        print("\nUppercase letters will not be included in your password.")
 
-def specialCharacters():
-    pass
+    lowerLetters(num, options, optionsPicked, password)
 
-def assemble():
-    pass
+def lowerLetters(num, options, optionsPicked, password):
+    
+    yn = input("\nWould you like to include lowercase letters in your password? (y = yes, n = no)\n")
+
+    if yn.upper() == "Y":
+        print("\nLowercase letters will be included in your password.")
+        options.append(lowerLetters)
+        optionsPicked += 1
+
+    else:
+        print("\nLowercase letters will not be included in your password.")
+
+    numbers(num, options, optionsPicked, password)
+
+def numbers(num, options, optionsPicked, password):
+    
+    yn = input("\nWould you like to include numbers letters in your password? (y = yes, n = no)\n")
+
+    if yn.upper() == "Y":
+        print("\nNumbers will be included in your password.")
+        options.append(numbers)
+        optionsPicked += 1
+
+    else:
+        print("\nNumbers will not be included in your password.")
+
+    specialCharacters(num, options, optionsPicked, password)
+
+def specialCharacters(num, options, optionsPicked, password):
+    
+    yn = input("\nWould you like to include special characters in your password? (y = yes, n = no)\n")
+
+    if yn.upper() == "Y":
+        print("\nSpecial characters will be included in your password.")
+        options.append(specialCharacters)
+        optionsPicked += 1
+
+    else:
+        print("\nSpecial characters will not be included in your password.")
+
+    assemble(num, options, optionsPicked, password)
+
+def assemble(num, options, optionsPicked, password):
+    
+    if optionsPicked == 0:
+        print("Your password is completely empty. You didn't pick anything! :/")
+
+    else:
+        print("")
+        for y in range(4):
+            for i in range(num):
+                print(options)
+                a = random.choice(options)
+                password = password + a
+            print(password)
+            password = ""
 
 def main():
     
-    numLetters()
+    options = []
 
-    upperLetters()
+    optionsPicked = 0
 
-    lowerLetters()
+    password = ""
 
-    numbers()
-
-    specialCharacters()
-
-    assemble()
+    numLetters(options, optionsPicked, password)
 
 main()
