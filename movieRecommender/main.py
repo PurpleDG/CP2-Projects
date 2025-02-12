5ef9eb058b2234fa0a5abbea464029d7abbdd192
PurpleDG <evan.mccabe@ucas-edu.net>
2025-02-12 13:44:12 -0700

    I continued working on my movie recommender program, which can now print all movies in the csv file in an orderly manner.

--- movieRecommender/main.py
+++ movieRecommender/main.py
@@ -2,9 +2,37 @@
 
 import csv
 
+print("\nHello! I am the movie recommender.")
+
+def main(movies):
+    
+    choice = input("\nWhat would you like to do?\n1 = Get Movie Reccomendations\n2 = Display All Movies\n3 = Exit\n")
+
+    try:
+        choice = int(choice)
+    except ValueError:
+        print("\nINVALID INPUT\n\nPlease try again.")
+        main(movies)
+
+    if choice == 1:
+        pass
+
+    elif choice == 2:
+        for row in movies:
+            #Title,Director,Genre,Rating,Length (min),Notable Actors
+            print("\nTitle: " + row[0] + " | Director: " + row[1] + " | Genre: " + row[2] + " | Rating: " + row[3] + " | Length(minutes): " + row[4] + " | Notable Actors: " + row[5])
+
+    elif choice == 3:
+        pass
+
+    else:
+        pass
+
 with open("movieRecommender\movies.csv") as file:
+
     movies = csv.reader(file)
-    #Remove the first line(it's just a guide):
+
+    # Remove the first line(it's just a guide):
     next(movies)
-    for row in movies:
-        print(row)
+
+    main(movies)
