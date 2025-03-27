11b93bb736aa97e8000485251eb64ed9d3d51615
PurpleDG <evan.mccabe@ucas-edu.net>
2025-03-27 13:45:15 -0600

    kept working

--- coinChangeProblem/main.py
+++ coinChangeProblem/main.py
@@ -9,7 +9,7 @@ print("\nHello! I am the coin change program. I can take a money amount and tell
 def main():
 
     #Ask the user what country they want to coin change money from:
-    country = input("\nWhat country do you want to coin change money from?\n1. America\n2. Japan\n")
+    country = input("\nWhat country do you want to coin change money from?\n1. America\n2. Japan\n3. France\n")
 
     #Make sure that the user entered a number:
     try:
@@ -19,7 +19,7 @@ def main():
         main()
 
     #Inner function to perform a coin change:
-    def coinChange(country, money):
+    def coinChange(country, money, rownum):
 
         print("\nThe country is ", country, ", and the amount of money to be coin changed is ", money, ".")
 
@@ -55,17 +55,23 @@ def main():
 
             for row in coins:
                 if row[0] == country:
+                    denominationNum = ((rownum-1)//2)
+                    a = 2
+                    for i in range(denominationNum):
 
-                    if money / float(row[2]) >= 1:
-                        numberNeeded = int(money // float(row[2]))
-                        print(numberNeeded, " ", row[1], "(s).")
-                        
+                        if money / float(row[i+a]) >= 1:
+                            numberNeeded = int(money // float(row[i+a]))
+                            if numberNeeded != 0:
+                                print(numberNeeded, " ", row[i+(a-1)], "(s).")
+                                money -= (int(numberNeeded)*float(row[i+a]))
+
+                        a += 1
 
         #Ask the user if they want to keep using the program:
         keepRunning()
 
     #Function to ask the user how much money they want to coin change:
-    def howMuchMoney():
+    def howMuchMoney(country):
 
         #Ask the user how much money they want to coin change:
         money = input("\nHow much money do you want to coin change? (number only, up to 2 decimal places)\n")
@@ -84,6 +90,12 @@ def main():
         else:
             print("\nTOO MANY DECIMAL PLACES\n\nPlease try again.")
             main()
+
+        #Make sure the user didn't input a number with a decimal for a country that doesn't have smaller money units:
+        if country == "Japan":
+            if money%1 != 0:
+                print("\nThat country doesn't do decimal amounts of money!\n\nPlease try again.")
+                main()
         
         #Return the user's desired amount of money to be coin changed for the coin change function:
         return money
@@ -92,13 +104,19 @@ def main():
     if country == 1:
         
         #Run the coin change function after asking the user how much money they want to coin change:
-        coinChange("America", howMuchMoney())
+        coinChange("America", howMuchMoney("America"), 25)
 
     #If the user picked Japan:
     elif country == 2:
         
         #Run the coin change function after asking the user how much money they want to coin change:
-        coinChange("Japan", howMuchMoney())
+        coinChange("Japan", howMuchMoney("Japan"), 19)
+
+    #If the user picked France:
+    elif country == 3:
+
+        #Run the coin change function after asking the user how much money they want to coin change:
+        coinChange("France", howMuchMoney("France"), 31)
 
     #If the user did not pick a valid option
     else:
