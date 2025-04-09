22e5e985cb0cf9499210fc005584fdb347f8b5db
PurpleDG <evan.mccabe@ucas-edu.net>
2025-04-08 21:26:42 -0600

    I kept working on my file updater program, but there are errors...

--- wordCounter/addToFile.py
+++ wordCounter/addToFile.py
@@ -1,4 +1,53 @@
+#Function to update a text file with a word count and date that it was updated:
 def update(path, time):
+
+    #Inner function to allow the user to choose whether or not to keep using the program:
+    def keepRunning():
+
+        #Ask the user if they want to keep using the program:
+        yn = input("\nWould you like to keep using this program? (y/n)\n")
+
+        #If they choose yes:
+        if yn == "y":
+
+            #Allow the while loop to continue making the program run:
+            return "Yes"
+
+        #If they choose no:
+        elif yn == "n":
+
+            #Say goodbye to the user:
+            print("\nBye-bye!\n")
+
+            #return no so that the main function can quit the program:
+            return "No"
+
+        #If the user does not choose yes or no:
+        else:
+
+            #Tell the user that they've made a mistake:
+            print("\nINVALID INPUT\n\nPlease try again.")
+
+            #Let the user try again:
+            keepRunning()
     
-    with open(path, "r+") as file:
-        file.
+    #Open the user's desired file so that it can be updated:
+    try:
+        with open(path, "a+") as file:
+
+            #Save the amount of words in the file as a variable:
+            fileContents = file.read()
+            wordCount = fileContents.split()
+
+            #Update the user's desired file with a word count and "date updated":
+            file.write("\nUPDATE: word count = " + str(wordCount) + ", date/time updated = " + str(time))
+
+            #Tell the user that the function was successful:
+            print("\nFile successfully updated!")
+
+            #Ask the user if they want to keep using the program:
+            return keepRunning()
+
+    #If the relative path entered by the user doesn't lead to a valid file:
+    except:
+        print("\nThat relative path doesn't lead to a valid file!\n\nPlease try again.")
--- wordCounter/main.py
+++ wordCounter/main.py
@@ -1,5 +1,7 @@
 #Evan McCabe
 
+import time
+
 #OVERVIEW:
 
 #Write a program that look at a document that a user has written on and update it with the word count and a timestamp for when that wordcount was last updated
@@ -13,17 +15,22 @@
 #Uses good naming practices
 #Has good white space
 
+#Fetch functions from other pages of the program to be used in the main page:
 from trackTime import getTime
-
 from addToFile import update
 
 #Introduce the program to the user:
 print("\nI am the file updater bot! I can add a word count to a document, along with a timestamp showing when it was updated.")
 
-def main():
+#Use all functions from other pages to run the entirety of the program:
+while True:
 
+    #Ask the user for the relative path of the file they want to update:
     path = input("\nWhat is the relative path of the file you would like to update?\n")
 
-    update(path, getTime())
+    #Update the user's desired text file with a word count and "date updated" using the update function:
+    keepGoing = update(path, getTime())
 
-main()
+    #Keep the program running or stop it based on the user's choice:
+    if keepGoing == "No":
+        break
