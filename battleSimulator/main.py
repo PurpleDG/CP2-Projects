ff385f6ea5ebb148b572d741f6f256ab2eae11c5
PurpleDG <evan.mccabe@ucas-edu.net>
2025-04-30 22:47:27 -0600

    It's working so far... But it's taking a long time...

--- battleSimulator/main.py
+++ battleSimulator/main.py
@@ -1,5 +1,7 @@
 #Evan McCabe Battle Simulator
 
+import csv
+
 loadedCharacters = []
 
 #When creating characters, give them an additional attribute to check if they were created by the user,
@@ -21,12 +23,12 @@ def checkNum(num):
 def main():
     
     #Ask the user what they want the program to do:
-    choice = input("\nWhat would you like to do?\n1 = Create Character\n2 = Save Character\n3 = Load Character\n4 = Your Characters' Stats\n5 = BATTLE\n")
+    choice = input("\nWhat would you like to do?\n1 = Create Character\n2 = Save Character\n3 = Load Character\n4 = Your Characters' Stats\n5 = BATTLE\n6 = Exit\n")
 
     choice = checkNum(choice)
 
     #Make sure the user input a valid option:
-    if choice > 0 and choice < 6:
+    if choice > 0 and choice < 7:
         pass
     else:
         print("\nINVALID OPTION\n\nPlease try again.")
@@ -62,6 +64,8 @@ def main():
 
         unsavedCharacters = False
 
+        print("\nHere are your current characters:")
+
         for x in loadedCharacters:
 
             if x["saved"] == False:
@@ -75,10 +79,16 @@ def main():
             toSaveName = input("\nWhat is the name of the character you would like to save?\n")
             for x in loadedCharacters:
                 if x["name"] == toSaveName:
+
                     validName = True
-                    fileLocation = input("\nWhat is the relative path of the file you want to save the charcter to?\n")
+
+                    characterToSave = [x["name"], x["attack"], x["defense"], x["speed"], x["level"], x["HP"], "True"]
+
+                    with open("battleSimulator\characters.csv", "a", newline='') as file:
                     
-                    #SAVE THE CHARACTER!!!!!!!
+                        charactersFile = csv.writer(file)
+
+                        charactersFile.writerow(characterToSave)
             
             if validName == False:
                 print("\nThat is not one of your unsaved characters!")
@@ -87,7 +97,23 @@ def main():
 
     #If the user chooses to load a character:
     if choice == 3:
-        pass
+        
+        print("\nHere are your saved characters:\n")
+
+        with open("battleSimulator\characters.csv", "r", newline='') as file:
+                    
+            charactersFile = csv.reader(file)
+
+            next(charactersFile)
+
+            for row in charactersFile:
+
+                print(f"Name: {row[0]} | Attack: {row[1]} | Defense: {row[2]} | Speed: {row[3]} | Level: {row[4]} | HP: {row[5]}")
+
+
+#CONTINUE WORKING HERE ^^^
+
+
 
     #If the user chooses to print character stats:
     if choice == 4:
@@ -96,6 +122,11 @@ def main():
     #If the user chooses to battle:
     if choice == 5:
         pass
+
+    #If the user chooses to exit:
+    if choice == 6:
+        print("\nGoodbye!\n")
+        exit()
 
     #Keep the program running in a loop:
     main()
