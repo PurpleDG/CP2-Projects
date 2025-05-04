a9dae861c1ba1b20a37a04a7ace3a4d9a5ab1ff1
PurpleDG <evan.mccabe@ucas-edu.net>
2025-05-03 19:20:23 -0600

    Working on things...

---
+++ personalPortfolio.py
@@ -0,0 +1,9 @@
+#Evan McCabe Personal Portfolio
+
+#Import the programs that will be included in the portfolio:
+from movieRecommender.main import main as recommendMovies
+
+
+
+keepGoing = True
+recommendMovies(keepGoing)
